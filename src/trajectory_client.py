f2911b3bffebc51cb50816e2bebbd6d849881ca3
Andy Wilson <adwilson10@gmail.com>
2015-06-16 16:33:30 -0500

    more updates

--- src/trajectory_client.py
+++ src/trajectory_client.py
@@ -3,7 +3,7 @@ import baxter_interface
 from baxter_interface import CHECK_VERSION
 
 from trajectory_msgs.msg import JointTrajectoryPoint
-from geometry_msgs.msg import Point
+from geometry_msgs.msg import Pose
 import math
 import numpy as np
 
@@ -35,17 +35,21 @@ class TrajectoryPublisher(object):
         self.ikl = ik_lookup_util.IKLookupUtil()
 
 
+    def _get_current_position(self, joint_names):
+        return [self._limb.joint_angle(joint) for joint in joint_names]
+
+
     def get_point(self, time):
         point = JointTrajectoryPoint()
         point.time_from_start = rospy.Duration.from_sec(time)
 
         #using sine function for test
-        refpose = Point()
-        refpose.position.y = 0.25*math.cos(time)-0.25
+        refpose = Pose()
+        refpose.pose.position.y = 0.25*math.cos(time)-0.25
         
         # get configs given reference pose:
         point.positions = self.ikl.ik_lookup(refpose)
-
+        
         # set velocity from inverse jacobian
         jref = self.ikl.jacob_lookup(refpose)
         point.velocities = np.dot(jref, np.array([0,-0.25*math.sin(time),0,0,0,0]))
@@ -56,42 +60,58 @@ class TrajectoryPublisher(object):
         return point
 
 
-    def _reorder_joints_ff_cmd(self, joint_names, point):
-        joint_name_order = self._limb.joint_names()
-        pnt = JointTrajectoryPoint()
-        pnt.time_from_start = point.time_from_start
-        pos_cmd = dict(zip(joint_names, point.positions))
-        for jnt_name in joint_name_order:
-            pnt.positions.append(pos_cmd[jnt_name])
-            if point.velocities:
-            vel_cmd = dict(zip(joint_names, point.velocities))
-            for jnt_name in joint_name_order:
-                pnt.velocities.append(vel_cmd[jnt_name])
-            if point.accelerations:
-            accel_cmd = dict(zip(joint_names, point.accelerations))
-            for jnt_name in joint_name_order:
-                pnt.accelerations.append(accel_cmd[jnt_name])
-            return pnt
-
     def start(self):
-        self.start_time = rospy.Time.now()
 
+        # move right arm to initial config:
+        rospy.loginfo("Moving right arm to initial condition")
+        p0 = Pose()
+        p0.pose.position.y = -0.5
+        q0 = self.ikl.ik_lookup(p0)
+        self.right_arm.move_to_joint_positions(q0)
+        self.qref_prev = q0
+        rospy.loginfo("Arms all in place, ready to start controller")
+
+        # register start time
+        self.start_time = rospy.Time.now()
+        self.end_time = 5.0
+        joint_names = self._limb.joint_names()
+
+        # execute trajectory
+        while ((rospy.Time.now() - self.start_time).to_sec() < self.end_time):
+            point = self.get_point((rospy.Time.now() - self.start_time).to_sec())
+
+            cmd = dict(zip(joint_names, point.positions))
+
+            self._limb.set_joint_positions(cmd, raw=True)
+            self._pub_ff_cmd.publish(point)
+
+            rospy.sleep(1.0 / self._control_rate)
+
+        # stop at final trajectory point
+        point.time_from_start = rospy.Duration(rospy.get_time() - start_time)
+        point.positions = self._get_current_position(joint_names)
+        point.velocities = [0.0] * len(joint_names)
+        point.accelerations = [0.0] * len(joint_names)
+        self._limb.set_joint_positions(joint_angles, raw=True)
+        self._pub_ff_cmd.publish(point)
+
+        return 0
 
 
 def main():
 
-    print("Initializing node... ")
+    rospy.loginfo("Initializing node... ")
     rospy.init_node("baxter_joint_publisher")
-    print("Getting robot state... ")
+    rospy.loginfo("Getting robot state... ")
     rs = baxter_interface.RobotEnable(CHECK_VERSION)
-    print("Enabling robot... ")
+    rospy.loginfo("Enabling robot... ")
     rs.enable()
-    print("Running. Ctrl-c to quit")
+    rospy.loginfo("Running. Ctrl-c to quit")
 
     traj = TrajectoryPublisher()
     traj.start()
 
-    print("Trajectory Execution Complete!")
+    rospy.loginfo("Trajectory Execution Complete!")
 
 
 if __name__ == "__main__":
